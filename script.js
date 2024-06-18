d27c389e3966bbf798c1725e7948d59052ba71de
mahamfathy <mahafathyh@gmail.com>
2024-06-18 20:48:39 +0300

    trying the animate css

--- script.js
+++ script.js
@@ -6,12 +6,15 @@ $(document).ready(()=>{
 
     $(`.third`).css('color', 'red')
     $("#btn_hide").click(()=>{
-        $('#second').hide(2000)
-        $('.third').toggle(2000)
-        $('.four').fadeOut()
-        $('.five').fadeToggle()
-        $('.six').fadeTo("slow",0.5)
-        $('.seven').slideToggle("fast")
+        // $('#second').hide(2000)
+        // $('.third').toggle(2000)
+        // $('.four').fadeOut()
+        // $('.five').fadeToggle()
+        // $('.six').fadeTo("slow",0.5)
+        // $('.seven').slideToggle("fast")
+        // $('.box').animate({
+        //     height: '300px',
+        //     width: '+=300px',fontSize:"50px"},2000)
     })
     $('body').keydown((e)=>{
     console.log(e.which)
