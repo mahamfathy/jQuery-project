7b06fa36ba7a89feb70f857adc3d7abfcebc93ea
mahamfathy <mahafathyh@gmail.com>
2024-06-18 19:31:37 +0300

    some sunctions

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 console.log("Welcome to The Ultimate jQuery Course!🚀");
-$('#second').hide()
+// $(()=>{
+//     console.log("jQuery Loaded");    
+// }) we can use the shorthand as well
+$(document).ready(()=>{
 
-$(`.third`).css('color', 'red')
+    $(`.third`).css('color', 'red')
+    $("#btn_hide").click(()=>{
+        $('#second').hide(2000)
+        $('.third').toggle(2000)
+    })
+    $('body').keydown((e)=>{
+    console.log(e.which)
+    if (e.which === 72) {
+        $('#second').hide()
+    }if (e.which === 83) {
+        $('#second').show()
+        
+    }
+    })
+})
